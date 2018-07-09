ef5ec618765e2dbd86ca1b8ee43b51eba7ce43e4
jamesjoel <james.joelj@gmail.com>
2018-07-09 01:06:35 -0700

    login auth

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var routes = express.Router();
 
-routes.use("/", require("../controller/home"));
+routes.use("/home", backdoor, require("../controller/home"));
+routes.use("/", require("../controller/login"));
 routes.use("/studentWebService", require("../controller/studentWebService"));
+routes.use("/loginWebService", require("../controller/loginWebService"));
 routes.use("/demoWebService", require("../controller/demoWebService"));
 
+
+function backdoor(req, res, next){
+	if(! req.session.is_user_logged_in){
+		res.redirect("/");
+	}
+	next();
+}
 
 module.exports=routes;
